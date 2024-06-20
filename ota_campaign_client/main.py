305be9c3835f6b21947d289aa858f7ae57216777
duskje <duskje@protonmail.com>
2024-06-20 14:27:09 -0400

    chore: write main event loop

--- ota_campaign_client/main.py
+++ ota_campaign_client/main.py
@@ -1,10 +1,22 @@
+import time
+import os
+
 from packaging.rpm import RPM
 
 from loguru import logger
 
+from dotenv import load_dotenv
+
 
 def main():
-    logger.debug(RPM().get_current_package_version('openssl'))
+    load_dotenv()
+    polling_interval_secs = float(os.getenv('POLLING_INTERVAL_SECS'))
+
+    while True:
+        logger.info('Polling from OTA Campaign server...')
+        time.sleep(polling_interval_secs)
+
+    # logger.debug(RPM().get_current_package_version('openssl'))
 
 
 if __name__ == '__main__':
